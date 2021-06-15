3ed429f9833cd744ad1383a080671c8f7d1781b6
BootCampBL <satyabhandari43@gmail.com>
2021-06-15 19:20:47 +0530

    Attendance check

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,13 @@
 #! /bin/bash -x
 
 echo "welcome to empcomputation"
+#! /bin/bash -x
+
+a=$(( RANDOM % 2 ))
+
+if [ $a -eq 1 ]
+then
+        echo "emp present"
+else
+        echo "emp absent"
+fi
